4a170cdfb95f818c9cae9882fb0f177aca4bf545
yxz6 <yxzbox@gmail.com>
2023-04-03 17:06:57 -0400

    add simple-file-server

---
+++ simple-file-server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go.olapie.com/rpcx/httpx"
+	"log"
+	"math/rand"
+	"net"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	var addr string
+	var dir string
+	flag.StringVar(&addr, "addr", "127.0.0.1:0", "address")
+	flag.StringVar(&dir, "dir", ".", "directory")
+	flag.Parse()
+
+	user := fmt.Sprint(rand.Int31() % 1e3)
+	password := fmt.Sprint(rand.Int31() % 1e3)
+	fmt.Println("User:", user)
+	fmt.Println("Password:", password)
+	userToAuthorization := httpx.CreateUserAuthorizations(map[string]string{user: password})
+
+	fs := http.FileServer(http.Dir(dir))
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("http://" + l.Addr().String())
+	err = http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		reqAuth := httpx.GetAuthorization(req.Header)
+		for u, auth := range userToAuthorization {
+			if auth == reqAuth {
+				log.Println(u)
+				fs.ServeHTTP(w, req)
+				return
+			}
+		}
+		w.Header().Set(httpx.KeyWWWAuthenticate, "Basic realm="+strconv.Quote("olapie"))
+		w.WriteHeader(http.StatusUnauthorized)
+	}))
+	log.Println(err)
+}
