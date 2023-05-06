737185a925feda15dc0423ad6dd71371e3ca7737
yxz6 <yxzbox@gmail.com>
2023-05-06 19:39:36 -0400

    fast commit

--- http4proxy/main.go
+++ http4proxy/main.go
@@ -1,53 +1,94 @@
 package main
 
 import (
-	"errors"
+	"crypto/tls"
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"net"
 	"net/http"
-	"net/http/httputil"
-	"net/netip"
-	"net/url"
-	"os"
-	"strings"
+	"time"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Printf("Usage: %s source target\n", os.Args[0])
-		return
+	var certFile string
+	var keyFile string
+	var proto string
+	var port int
+	flag.StringVar(&certFile, "cert", "server.pem", "cert filepath")
+	flag.StringVar(&keyFile, "key", "server.key", "key filepath")
+	flag.StringVar(&proto, "proto", "https", "Proxy protocol (http or https)")
+	flag.IntVar(&port, "port", 8080, "Proxy server port")
+	flag.Parse()
+	if proto != "http" && proto != "https" {
+		log.Fatal("Protocol must be either http or https")
 	}
 
-	source := os.Args[1]
-	if !strings.Contains(source, ":") {
-		source = "0.0.0.0:" + source
-	} else if source[0] == ':' {
-		source = "0.0.0.0" + source
+	server := &http.Server{
+		Addr: fmt.Sprintf(":%d", port),
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == http.MethodConnect {
+				handleTunneling(w, r)
+			} else {
+				handleHTTP(w, r)
+			}
+		}),
+		// Disable HTTP/2.
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
 	}
 
-	addr, err := netip.ParseAddrPort(source)
-	if err != nil {
-		fmt.Println("invalid source", os.Args[1], err)
-		return
-	}
-
-	target := os.Args[2]
-	if !strings.Contains(target, ":") {
-		target = "http://127.0.0.1:" + target
-	} else if source[0] == ':' {
-		target = "http://127.0.0.1" + target
-	}
-
-	targetURL, err := url.Parse(target)
-	if err != nil {
-		fmt.Println("invalid target", os.Args[2])
-		return
-	}
-
-	fmt.Printf("reverse proxy: http://%v -> %v\n", addr, targetURL)
-	err = http.ListenAndServe(addr.String(), httputil.NewSingleHostReverseProxy(targetURL))
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Println("closed")
+	if proto == "http" {
+		log.Fatal(server.ListenAndServe())
 	} else {
-		fmt.Println(err)
+		log.Fatal(server.ListenAndServeTLS(certFile, keyFile))
+	}
+}
+
+func handleTunneling(w http.ResponseWriter, r *http.Request) {
+	destConn, err := net.DialTimeout("tcp", r.Host, 10*time.Second)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
+	}
+
+	clientConn, _, err := hijacker.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	}
+	go transfer(destConn, clientConn)
+	go transfer(clientConn, destConn)
+}
+
+func transfer(destination io.WriteCloser, source io.ReadCloser) {
+	defer destination.Close()
+	defer source.Close()
+	io.Copy(destination, source)
+}
+
+func handleHTTP(w http.ResponseWriter, req *http.Request) {
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer resp.Body.Close()
+	copyHeader(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
 	}
 }
